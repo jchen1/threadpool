9cccf817077732faca030622a04eaedc83960229
Jeff Chen <jeffrey@cmu.edu>
2013-06-14 16:18:17 -0300

    Add platform-independent code from Windows version

--- WorkerThread.hpp
+++ WorkerThread.hpp
@@ -5,14 +5,17 @@
 namespace threadpool {
 	
 	using namespace std;
+	class ThreadPool;
 
-	template <typename Task, typename ThreadPool>
+	template <typename Task>
 	class WorkerThread
 	{
 	public:
-		WorkerThread()
+		WorkerThread(shared_ptr<ThreadPool> pool) :
+			p_pool(pool)
 		{
-
+			//create thread
+			m_thread(run);
 		}
 
 		~WorkerThread() {}
@@ -24,6 +27,31 @@ namespace threadpool {
 
 	private:
 		thread m_thread;
+		shared_ptr<ThreadPool> p_pool;
+		unique_ptr<Task> p_currentTask;
+		
+		void run()
+		{
+			++p_pool->m_threadsPending;
+			while (p_pool->isRunning())
+			{
+				if (p_pool->tasksLeft())
+				{
+					p_currentTask = p_pool->getNextTask();
+					
+					if (p_currentTask.get() != nullptr)
+					{
+						--p_pool->m_threadsPending;
+						++p_pool->m_threadsRunning;
+						p_pool->m_tasksCompleted.push((*p_currentTask)());
+						--p_pool->m_threadsRunning;
+						++p_pool->m_threadsPending;
+					}
+				}
+				
+				//sleep(20);
+			}
+		}
 
 	};
 
