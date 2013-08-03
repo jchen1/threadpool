914885dfe338c17d1ace272f5518905b1faf3898
Jeff Chen <jeffrey@cmu.edu>
2013-08-02 20:34:54 -0400

    Reset

--- ThreadPool.hpp
+++ ThreadPool.hpp
@@ -1,108 +1,12 @@
 #ifndef THREADPOOL_THREADPOOL_H
 #define THREADPOOL_THREADPOOL_H
 
-#include <vector>
-#include <queue>
-#include <functional>
-#include <mutex>
-#include <algorithm>
-#include <thread>
 
-#include "WorkerThread.hpp"
 
 class ThreadPool
 {
-public:
-
-	typedef std::function<void*(void)> Task;
-	typedef WorkerThread<Task> Thread;
-
-	ThreadPool(int maxThreads) :
-		m_maxThreads(maxThreads),
-		m_threadsCreated(0),
-		m_bRunning(true),
-		m_bTasksLeft(false),
-		m_threadsRunning(0),
-		m_threadsPending(0)
-	{
-		p_threads.reserve(m_maxThreads);
-	}
 
 
-	~ThreadPool()
-	{
-		joinAll();
-	}
-	
-	void addTask(Task t)
-	{
-		if (m_threadsPending == 0 && m_threadsCreated < m_maxThreads)
-		{
-			p_threads.emplace_back(new Thread(this));
-			++m_threadsCreated;
-		}
-		m_taskQueueMutex.lock();
-		p_taskQueue.push(std::unique_ptr<Task>(new Task(t)));
-		m_taskQueueMutex.unlock();
-		m_bTasksLeft = true;
-	}
-	
-	void* getCompletedTask()
-	{
-		if (p_tasksCompleted.empty())
-		{
-			return nullptr;
-		}
-		
-		void* task = p_tasksCompleted.front();
-		p_tasksCompleted.pop();
-		
-		return task;
-	}
-	
-	inline bool tasksLeft() { return m_bTasksLeft; }
-	inline bool isRunning() { return m_bRunning; }
-
-	void joinAll()
-	{
-		while (tasksLeft())
-		{
-			std::this_thread::sleep_for(chrono::milliseconds(100));
-		}
-		for_each(std::begin(p_threads), std::end(p_threads), std::bind(Thread::join, std::placeholders::_1));
-	}
-
-private:
-	
-	std::vector<std::unique_ptr<Thread>> p_threads;
-	std::queue<std::unique_ptr<Task>> p_taskQueue;
-	std::queue<void*> p_tasksCompleted;
-	std::mutex m_taskQueueMutex;
-	bool m_bTasksLeft, m_bRunning;
-	int m_maxThreads, m_threadsCreated;
-	
-	volatile int m_threadsRunning, m_threadsPending;
-	
-	std::unique_ptr<Task> getNextTask()
-	{
-		std::unique_ptr<Task> task;
-		m_taskQueueMutex.lock();
-		if (tasksLeft())
-		{
-			task = move(p_taskQueue.front());
-			p_taskQueue.pop();
-			if (p_taskQueue.empty())
-			{
-				m_bTasksLeft = false;
-			}
-		}
-		else
-		{
-			m_bTasksLeft = false;
-		}
-		m_taskQueueMutex.unlock();
-		return task;
-	}
 
 
 };
--- WorkerThread.hpp
+++ WorkerThread.hpp
@@ -1,55 +1,10 @@
 #ifndef THREADPOOL_WORKERTHREAD_H
 #define THREADPOOL_WORKERTHREAD_H
 
-#include <thread>
-#include <chrono>
-#include "ThreadPool.hpp"
 
-template <typename Task>
 class WorkerThread
 {
-public:
-	WorkerThread<Task>(ThreadPool* pool) :
-		p_pool(pool)
-	{
-		//create thread
-		m_thread(std::bind(&WorkerThread::run, this));
-	}
 
-	~WorkerThread() {}
-
-	void join()
-	{
-		m_thread.join();
-	}
-
-private:
-	std::thread m_thread;
-	ThreadPool* p_pool;
-	std::unique_ptr<Task> p_currentTask;
-	
-	void run()
-	{
-		++p_pool->m_threadsPending;
-		while (p_pool->isRunning())
-		{
-			if (p_pool->tasksLeft())
-			{
-				p_currentTask = p_pool->getNextTask();
-				
-				if (p_currentTask.get() != nullptr)
-				{
-					--p_pool->m_threadsPending;
-					++p_pool->m_threadsRunning;
-					p_pool->m_tasksCompleted.push((*p_currentTask)());
-					--p_pool->m_threadsRunning;
-					++p_pool->m_threadsPending;
-				}
-			}
-			
-			std::this_thread::sleep_for(std::chrono::milliseconds(20));
-		}
-	}
 
 };
 
